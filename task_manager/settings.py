f7d2980bee53e121b9697c5de12482b933c6127b
Sergei <reyangood@gmail.com>
2024-03-09 00:18:08 +0300

    add '.onrender.com' to ALLOWED_HOSTS in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'webserver']
+ALLOWED_HOSTS = ['127.0.0.1', 'webserver', '.onrender.com']
 
 
 # Application definition
