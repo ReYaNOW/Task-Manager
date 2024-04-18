dbf510892cefec466ff7ac071519345e44ad3e35
Sergei <reyangood@gmail.com>
2024-04-18 18:51:25 +0300

    change executor view from username to get_full_name() in tasks/forms.py

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,13 +1,19 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.utils.encoding import smart_str
 from django.utils.translation import gettext_lazy as _
 
-from django.contrib.auth.models import User
-from task_manager.statuses.models import Status
-from task_manager.labels.models import Label
 from .models import Task
 
 
+class UserFullnameChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, user):
+        return smart_str(user.get_full_name())
+
+
 class TaskCreateForm(forms.ModelForm):
+    executor = UserFullnameChoiceField(queryset=User.objects.all())
+
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
