88569e7ae63acf7825abb0a7640c2aee5c5eb652
Sergei <reyangood@gmail.com>
2024-04-18 19:42:55 +0300

    fix linter error

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import User
 
 from task_manager.users.models import UserFullName
 from task_manager.statuses.models import Status
