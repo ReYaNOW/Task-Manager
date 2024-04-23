8b7dd4d8c5ce1e9ec08534ba5baca2da44d49bfd
Sergei <reyangood@gmail.com>
2024-04-23 18:08:43 +0300

    add fix for CI/CD in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -93,8 +93,15 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {'default': dj_database_url.config()}
+if os.getenv('DATABASE_URL'):
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        },
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
